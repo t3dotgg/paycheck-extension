e095bc87fcc2bbb870212c4011a035fd221b7a6c
Erwin <erwin@remotedev.co>
2023-08-09 16:32:06 +0800

    Fix issue #4

--- main.js
+++ main.js
@@ -22,17 +22,54 @@ function convertToDollars(number) {
 
 const globalSelectors = {};
 globalSelectors.postCounts = `[role="group"][id*="id__"]:only-child`;
-globalSelectors.viewCount = globalSelectors.postCounts + " a[href*='/analytics']";
+globalSelectors.articleDate = `[role="article"][aria-labelledby*="id__"][tabindex="-1"] time`;
+globalSelectors.analyticsLink = " a[href*='/analytics']";
+globalSelectors.viewCount = globalSelectors.postCounts + globalSelectors.analyticsLink;
 
 const innerSelectors = {};
 innerSelectors.dollarSpot = "div div:first-child";
 innerSelectors.viewSVG = "div div:first-child svg";
 innerSelectors.viewAmount = "div div:last-child span span span";
+innerSelectors.articleViewAmount = "span div:first-child span span span";
 
 function doWork() {
   const viewCounts = Array.from(
     document.querySelectorAll(globalSelectors.viewCount)
   );
+
+  const articleViewDateSection = document.querySelector(globalSelectors.articleDate);
+
+  if(articleViewDateSection) {
+    let rootDateViewsSection = articleViewDateSection.parentElement.parentElement.parentElement;
+
+    if(rootDateViewsSection?.children.length === 1) {
+      // we're dealing with the <time> element on a quote retweet
+      // do globalSelector query again but with 2nd result
+      rootDateViewsSection = document.querySelectorAll(globalSelectors.articleDate)[1].parentElement.parentElement.parentElement;
+    }
+
+    // if there are more than 4, we already added the paycheck value
+    if(rootDateViewsSection?.children.length < 4) {
+
+      // clone 2nd and 3rd child of rootDateViewsSection
+      const clonedDateViewSeparator = rootDateViewsSection?.children[1].cloneNode(true);
+      const clonedDateView = rootDateViewsSection?.children[2].cloneNode(true);
+
+      // insert clonedDateViews and clonedDateViewsTwo after the 3rd child we just cloned
+      rootDateViewsSection?.insertBefore(clonedDateViewSeparator, rootDateViewsSection?.children[2].nextSibling);
+      rootDateViewsSection?.insertBefore(clonedDateView, rootDateViewsSection?.children[3].nextSibling);
+
+      // get view count value from 'clonedDateViewsTwo'
+      const viewCountValue = clonedDateView?.querySelector(innerSelectors.articleViewAmount)?.textContent;
+      const dollarAmount = convertToDollars(viewCountValue);
+
+      // replace textContent in cloned clonedDateViews (now 4th child) with converted view count value
+      clonedDateView.querySelector(innerSelectors.articleViewAmount).textContent = "$" + dollarAmount;
+
+      // remove 'views' label
+      clonedDateView.querySelector(`span`).children[1].remove()
+    }
+  }
 
   viewCounts.map((view) => {
     // Early escape
