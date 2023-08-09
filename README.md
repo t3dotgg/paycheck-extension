0661861b997657820d392745f8f568cbaf8202f0
Theo Browne <me@t3.gg>
2023-08-08 19:35:05 -0700

    working

---
+++ README.md
@@ -0,0 +1,3 @@
+# PayCheck for Twitter
+
+tl;dr - I made an extension that (poorly) estimates how much money a tweet is worth
---
+++ main.js
@@ -0,0 +1,113 @@
+function convertFromTwitter(number) {
+  const cleaned = number.replace(/,/g, "");
+  var base = parseFloat(cleaned);
+  if (number.toLowerCase().match(/k/)) {
+    return Math.round(base * 1000);
+  } else if (number.toLowerCase().match(/m/)) {
+    return Math.round(base * 1000000);
+  } else if (number.toLowerCase().match(/b/)) {
+    return Math.round(base * 1000000000);
+  } else {
+    return base;
+  }
+}
+
+function convertToDollars(number) {
+  const rawCount = convertFromTwitter(number);
+
+  const processed = rawCount * 0.000026;
+  if (processed < 0.1) return processed.toFixed(5);
+  return processed.toFixed(2);
+}
+
+const globalSelectors = {};
+globalSelectors.tweetCounts = `[role="group"][id*="id__"]:only-child`;
+globalSelectors.viewCount =
+  globalSelectors.tweetCounts + " a[href*='/analytics']";
+
+const innerSelectors = {};
+
+innerSelectors.dollarSpot = "div div:first-child";
+
+innerSelectors.viewSVG = "div div:first-child svg";
+
+innerSelectors.viewAmount = "div div:last-child span span span";
+
+function doWork() {
+  const viewCounts = Array.from(
+    document.querySelectorAll(globalSelectors.viewCount)
+  );
+
+  viewCounts.map((view) => {
+    // Early escape
+    if (view.classList.contains("replaced")) return;
+
+    // Make sure we don't touch this one again
+    view.classList.add("replaced");
+
+    // Remove view count icon
+    const oldIcon = view.querySelector(innerSelectors.viewSVG);
+    console.log("removing icon", oldIcon);
+    oldIcon?.remove();
+
+    // Get the number
+    const viewCount = view.querySelector(innerSelectors.viewAmount);
+    console.log("processing viewCount", viewCount);
+    const dollars = convertToDollars(viewCount.textContent);
+    viewCount.textContent = dollars;
+
+    // Swap the svg for a dollar sign
+    const dollarSpot = view.querySelector(innerSelectors.dollarSpot)?.firstChild
+      ?.firstChild;
+    console.log("adding dollar sign", dollarSpot);
+    dollarSpot.textContent = "$";
+
+    // Magic alignment value
+    dollarSpot.style.marginTop = "-0.6rem";
+  });
+}
+
+function throttle(func, limit) {
+  let lastFunc;
+  let lastRan;
+  return function () {
+    const context = this;
+    const args = arguments;
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(function () {
+        if (Date.now() - lastRan >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+}
+
+// Function to start MutationObserver
+let mt; // Mutations timeout
+const observe = () => {
+  console.log("starting observation");
+  const observer = new MutationObserver((mutationsList) => {
+    if (!mutationsList.length) return;
+
+    const runDocumentMutations = throttle(async () => {
+      doWork();
+
+      return;
+    }, 1000);
+
+    runDocumentMutations();
+  });
+
+  observer.observe(document, {
+    childList: true,
+    subtree: true,
+  });
+};
+
+observe();
