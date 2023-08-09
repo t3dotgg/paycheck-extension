9098190552f14a07f75a9f062f467a2f653c042e
Logan Garrett <ninetailsoftware@gmail.com>
2023-08-09 14:07:34 -0400

    Removed broken hover image.

--- main.js
+++ main.js
@@ -105,25 +105,21 @@ function doWork() {
     // Hover changes color.
     viewCount.addEventListener('mouseenter', function() {
       dollarSpot.style.color = '#FF69B4';
-      dollarSpot.style.boxShadow = '0 0 0 3px #FF69B4'; 
       this.style.color = '#FF69B4'; 
     });
 
     viewCount.addEventListener('mouseleave', function() {
       dollarSpot.style.color = baseColor;
-      dollarSpot.style.boxShadow = null;
       this.style.color = baseColor; 
     });
 
     dollarSpot.addEventListener('mouseenter', function() {
       viewCount.style.color = '#FF69B4';
-      this.style.boxShadow = '0 0 0 3px #FF69B4'; 
       this.style.color = '#FF69B4'; 
     });
 
     dollarSpot.addEventListener('mouseleave', function() {
       viewCount.style.color = baseColor;
-      this.style.boxShadow = null;
       this.style.color = baseColor;
     });
     
